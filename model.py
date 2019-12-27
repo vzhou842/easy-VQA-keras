facf2bc4d6b752442fcf7ac01651474cea66cfa3
Victor Zhou <vzhou842@gmail.com>
2019-12-27 17:14:02 +1000

    Remove full_model param for model structure

--- model.py
+++ model.py
@@ -3,30 +3,27 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Multiply
 from keras.optimizers import Adam
 from constants import *
 
-def build_model(im_shape, vocab_size, num_answers, full_model=False):
+def build_model(im_shape, vocab_size, num_answers):
   # The CNN
   im_input = Input(shape=im_shape)
   x1 = Conv2D(32, 3, padding='same')(im_input)
   x1 = MaxPooling2D()(x1)
-  if full_model:
-    x1 = Conv2D(32, 3, padding='same')(x1)
-    x1 = MaxPooling2D()(x1)
-    x1 = Conv2D(32, 3, padding='same')(x1)
-    x1 = MaxPooling2D()(x1)
+  x1 = Conv2D(32, 3, padding='same')(x1)
+  x1 = MaxPooling2D()(x1)
+  x1 = Conv2D(32, 3, padding='same')(x1)
+  x1 = MaxPooling2D()(x1)
   x1 = Flatten()(x1)
   x1 = Dense(32, activation='tanh')(x1)
 
-  #The question network
+  # The question network
   q_input = Input(shape=(vocab_size,))
   x2 = Dense(32, activation='tanh')(q_input)
   x2 = Dense(32, activation='tanh')(x2)
-  if full_model:
-    x2 = Dense(32, activation='tanh')(x2)
+  x2 = Dense(32, activation='tanh')(x2)
 
   # Merge -> output
   out = Multiply()([x1, x2])
-  if full_model:
-    out = Dense(32, activation='tanh')(out)
+  out = Dense(32, activation='tanh')(out)
   out = Dense(num_answers, activation='softmax')(out)
 
   model = Model(inputs=[im_input, q_input], outputs=out)
