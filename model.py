a90e50a7f90d3b61f66dfede387006bce4c2e8ed
Victor Zhou <vzhou842@gmail.com>
2019-12-29 22:41:03 +1100

    Reduce CNN size

--- model.py
+++ model.py
@@ -5,12 +5,12 @@ from keras.optimizers import Adam
 def build_model(im_shape, vocab_size, num_answers):
   # The CNN
   im_input = Input(shape=im_shape)
-  x1 = Conv2D(32, 3, padding='same')(im_input)
+  x1 = Conv2D(8, 3, padding='same')(im_input)
   x1 = MaxPooling2D()(x1)
-  x1 = Conv2D(32, 3, padding='same')(x1)
-  x1 = MaxPooling2D()(x1)
-  x1 = Conv2D(32, 3, padding='same')(x1)
-  x1 = MaxPooling2D()(x1)
+  # x1 = Conv2D(8, 3, padding='same')(x1)
+  # x1 = MaxPooling2D()(x1)
+  # x1 = Conv2D(8, 3, padding='same')(x1)
+  # x1 = MaxPooling2D()(x1)
   x1 = Flatten()(x1)
   x1 = Dense(32, activation='tanh')(x1)
 
