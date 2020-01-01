0e8438af6028334a3e8fc44bd3a784aeed4e41a5
Victor Zhou <vzhou842@gmail.com>
2020-01-02 10:24:39 +1100

    Increas lr to 5e-4

--- model.py
+++ model.py
@@ -24,6 +24,6 @@ def build_model(im_shape, vocab_size, num_answers):
   out = Dense(num_answers, activation='softmax')(out)
 
   model = Model(inputs=[im_input, q_input], outputs=out)
-  model.compile(Adam(lr=2e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+  model.compile(Adam(lr=5e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
   return model
