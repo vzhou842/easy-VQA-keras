98c2afef9692c3ca6f8de805da9c46aef9b05404
Victor Zhou <vzhou842@gmail.com>
2020-01-09 00:04:15 -0800

    Update epochs to 8, add comments

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import argparse
 from model import build_model
 from prepare_data import setup
 
+# Support command-line options
 parser = argparse.ArgumentParser()
 parser.add_argument('--big-model', action='store_true', help='Use the bigger model with more conv layers')
 parser.add_argument('--use-data-dir', action='store_true', help='Use custom data directory, at /data')
@@ -13,6 +14,7 @@ if args.big_model:
 if args.use_data_dir:
   print('Using data directory')
 
+# Prepare data
 train_X_ims, train_X_seqs, train_Y, test_X_ims, test_X_seqs, test_Y, im_shape, vocab_size, num_answers, _, _, _ = setup(args.use_data_dir)
 
 print('\n--- Building model...')
@@ -25,6 +27,6 @@ model.fit(
   train_Y,
   validation_data=([test_X_ims, test_X_seqs], test_Y),
   shuffle=True,
-  epochs=7,
+  epochs=8,
   callbacks=[checkpoint],
 )
